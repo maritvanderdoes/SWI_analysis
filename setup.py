80c9e8e504eae3478136d4e39365acd5a4db673c
Markus Rempfler <markus.rempfler@fmi.ch>
2021-02-09 15:56:12 +0100

    Add setup.py to simplify installation

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+contrib = ['Marit van der Does']
+
+# setup.
+setup(
+    name='swi-analysis',
+    version='0.0.0',
+    description='SWI analysis in python',
+    author=', '.join(contrib),
+    license='MIT',
+    packages=find_packages(exclude=[
+        'tests',
+    ]),
+    install_requires=[
+        'numpy>=1.19',
+        'matplotlib>=3.3',
+        'scikit-image>=0.17',
+        'scipy>=1.5',
+        'seaborn>=0.11',
+        'pandas>=1.1',
+    ],
+)
