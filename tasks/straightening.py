4321309ce542bfd5de092f170bb39862d33abfa9
maritvanderdoes <maritvddoes@gmail.com>
2021-04-21 15:15:34 +0200

    straigtening update

--- tasks/straightening.py
+++ tasks/straightening.py
@@ -35,6 +35,7 @@ def create_skeleton(twoDimage,twoDmask):
   fil.create_mask(use_existing_mask=True)
   fil.medskel(verbose=False)
   fil.analyze_skeletons(branch_thresh=40* u.pix, skel_thresh=10 * u.pix, prune_criteria='length')
+
   plt.figure()
   plt.imshow(np.max(img_mcherry,0), cmap='gray')
   plt.contour(fil.skeleton_longpath, colors='r')
@@ -50,7 +51,7 @@ def create_skeleton(twoDimage,twoDmask):
 
 
 #%% load parameters
-dirpath = '/Users/Marit/Documents/work/test_images/HBL1gfp_worm6'
+dirpath = '/Users/Marit/Documents/work/test_images'
 outputpath = 'Users/Marit/Documents'
 channel_GFP =   'w1Lucas-sim-488-561'
 channel_mcherry= 'w2Lucas-sim-561-488'
@@ -65,7 +66,7 @@ list_mcherry, list_GFP = image_lists_mcherry_GFP(dirpath, channel_mcherry, chann
 
 #open mcherry and segment on signal
 for i,(file1, file2) in enumerate(zip(list_mcherry, list_GFP)):
-    if (i==15):
+    if (i==0):
         print(file1)
 
         img_mcherry = read_image(file1)
@@ -79,39 +80,24 @@ for i,(file1, file2) in enumerate(zip(list_mcherry, list_GFP)):
         twoDmask=np.max(img_binary,0)
         X,Y=create_skeleton(twoDimage,twoDmask)
 
-       # %%
-        tckp, u = interpolate.splprep([X, Y], s=3, k=2, nest=-1)
-        xpointsnew, ypointsnew = interpolate.splev(np.linspace(0,1,270), tckp)
-
-        plt.figure(3)
-        plt.plot(xpointsnew, ypointsnew, 'r-')
-        plt.show()
 
 
-       # %%
-      #runs a nearest neighbors algorithm on the coordinate array or spline interpolation to order the coordinates
+        #create spline
+        # reformats x and y coordinates into a 2-dimensional array
+        inputarray = np.c_[X, Y]
 
-      
 
+#%%  create spline
+        # runs a nearest neighbors algorithm on the coordinate array
         from sklearn.neighbors import KNeighborsRegressor
-        neigh = KNeighborsRegressor(n_neighbors=2)
-        G = neigh.kneighbors_graph()
-        #neigh.fit(X,Y)
-        #print(neigh.predict([[1.5]]))
-      
-        
-  
-        # %%
-
         clf = NearestNeighbors(2).fit(inputarray)
-        
-       
+        G = clf.kneighbors_graph()
         T = nx.from_scipy_sparse_matrix(G)
 
         # sorts coordinates according to their nearest neighbors order
         order = list(nx.dfs_preorder_nodes(T, 0))
-        xx = input_x[order]
-        yy = input_y[order]
+        xx = X[order]
+        yy = Y[order]
 
         # Loops over all points in the coordinate array as origin, determining which results in the shortest path
         paths = [list(nx.dfs_preorder_nodes(T, i)) for i in range(len(inputarray))]
@@ -130,20 +116,63 @@ for i,(file1, file2) in enumerate(zip(list_mcherry, list_GFP)):
 
         opt_order = paths[minidx]
 
-        xxx = input_x[opt_order]
-        yyy = input_y[opt_order]
+        xxx = X[opt_order]
+        yyy = Y[opt_order]
 
         # fits a spline to the ordered coordinates
-        tckp, u = interpolate.splprep([xxx, yyy], s=3, k=2, nest=-1)
-        xpointsnew, ypointsnew = interpolate.splev(np.linspace(0,1,270), tckp)
+        nsplinepoints=200
 
-        # prints spline variables
-        print(tckp)
+        tckp, u = interpolate.splprep([xxx, yyy], s=3, k=2, nest=-1)
+        xpointsnew, ypointsnew = interpolate.splev(np.linspace(0,1,nsplinepoints), tckp)
+
+        
+
+        #calculating derivative
+        dx, dy = interpolate.splev(np.linspace(0,1,nsplinepoints), tckp,der=1)
+        mag=np.sqrt(dx**2+dy**2)
+        dx=dx/mag
+        dy=dy/mag
+
+        n =20
+        upperxcoord = xpointsnew - n*dy
+        upperycoord = ypointsnew + n*dx
+
+        lowerxcoord = xpointsnew + n*dy
+        lowerycoord = ypointsnew - n*dx
+
+        plt.figure(figsize=(10,10))
+        plt.plot(xpointsnew, ypointsnew, 'r-')
+        plt.plot(upperxcoord, upperycoord, 'b-')
+        plt.plot(lowerxcoord, lowerycoord, 'b-')
+
+     
 
         # plots the spline
-        plt.figure(3)
-        plt.plot(xpointsnew, ypointsnew, 'r-')
-        plt.show()
+       
+        xset = np.concatenate((xpointsnew,upperxcoord,lowerxcoord))
+        yset = np.concatenate((ypointsnew,upperycoord,lowerycoord))
+        oldcoord=[xset,yset]
+
+        xsetnew = np.concatenate((np.zeros(nsplinepoints),np.ones(nsplinepoints)*(-n),np.ones(nsplinepoints)*(n)))
+        ysetnew = np.concatenate((np.linspace(0,1,nsplinepoints),np.linspace(0,1,nsplinepoints),np.linspace(0,1,nsplinepoints)))
+        newcoord = [xsetnew,ysetnew]
+   
+        sq = 21
+        ysq  = 51
+        output_region=(-ysq,-sq,ysq,sq)
+        #output_region=(-1,-1,2,2)
+
+        img2 = np.max(img_mcherry,0)
+
+        test=warp_images(np.transpose(oldcoord), np.transpose(newcoord), img2[None,:,:], output_region, interpolation_order = 1, approximate_grid=2)
+        plt.figure()
+        plt.imshow(test[0])
+        #plt.imshow(np.max(img_mcherry,0))
+
+
+
+
+
 
 
 
